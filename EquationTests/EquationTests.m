2c66964585dbb50cc73359e03fd214949dc82fa2
hamashima shintaro <d30730sh@apps.kct.ac.jp>
2012-12-13 16:38:49 +0900

    書き換えた

--- EquationTests/EquationTests.m
+++ EquationTests/EquationTests.m
@@ -127,4 +127,28 @@ Equation* e;
 // 実は虚数や0で割り算した場合など，計算結果が実数で表せない値になってしまったとき
 // nan という特殊な数値になります。
 // それでテストがパスしてしまったのですね。
+
+// 自分で作った虚数解二つの場合
+- (void)test5
+{
+    e = [[Equation alloc] initWithA:2 b:4 c:4];
+    
+    STAssertEqualsWithAccuracy(-1.0, [e real1], 0.00001, @"real1 error");
+    STAssertEqualsWithAccuracy(-1.0, [e real2], 0.00001, @"real2 error");
+    STAssertEqualsWithAccuracy(1.0, [e imaginary1], 0.00001, @"imaginary1 error");
+    STAssertEqualsWithAccuracy(-1.0, [e imaginary2], 0.00001, @"imaginary2 error");
+    
+}
+
+
+//重解
+- (void)test6
+{
+    e=[[Equation alloc] initWithA:1 b:6 c:9];
+    
+    STAssertEqualsWithAccuracy(-3.0, [e real1], 0.00001, @"real1 error");
+    STAssertEqualsWithAccuracy(-3.0, [e real2], 0.00001, @"real2 error");
+    STAssertEqualsWithAccuracy(0.0, [e imaginary1], 0.00001, @"imaginary1 error");
+    STAssertEqualsWithAccuracy(0.0, [e imaginary2], 0.00001, @"imaginary2 error");
+}
 @end
